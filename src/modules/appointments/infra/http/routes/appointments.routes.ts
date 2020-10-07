eee429ee1751806b8f027dabffd4833fa0ddeb44
guiialmeiida <gui.almeidacrz@gmail.com>
2020-10-07 07:12:28 -0300

    Aplicando dependency inversion e principios do solid

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import { getCustomRepository } from 'typeorm';
 import { parseISO } from "date-fns";
+import { container } from 'tsyringe';
 
-import AppointmentsRepository from "@modules/appointments/repositories/AppointmentsRepository";
 import CreateAppointmentService from "@modules/appointments/services/CreateAppointmentService";
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
@@ -11,12 +10,11 @@ const appointmentsRouter = Router();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get("/", async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+/* appointmentsRouter.get("/", async (request, response) => {
   const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
-});
+}); */
 
 appointmentsRouter.post("/", async (request, response) => {
   try {
@@ -24,7 +22,7 @@ appointmentsRouter.post("/", async (request, response) => {
 
     const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService();
+    const createAppointment = container.resolve(CreateAppointmentService)
 
     const appointment = await createAppointment.execute({
       date: parsedDate,
--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { container } from 'tsyringe';
 
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
@@ -8,7 +9,7 @@ sessionsRouter.post("/", async (request, response) => {
   try {
     const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticateUserService();
+    const authenticateUser = container.resolve(AuthenticateUserService);
 
     const { user, token } = await authenticateUser.execute({
       email,
