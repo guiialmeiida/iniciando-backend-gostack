c2fabf7757b4306f6690e98ec3431ad8ce0a0f71
guiialmeiida <gui.almeidacrz@gmail.com>
2020-11-09 07:07:34 -0300

    Listando dias disponíveis

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,6 +1,8 @@
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import { fromString } from 'uuidv4';
 
 interface IRequest {
   provider_id: string;
@@ -27,7 +29,23 @@ class ListProviderMonthAvailabilityService {
       month,
     });
 
-    console.log(appointments);
+    const numberOfDaysInmonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInmonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
 
     return [{ day: 1, available: false }];
   }
