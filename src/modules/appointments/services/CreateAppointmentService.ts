d8f716e0af7404048179fa5dc5309edaa5ef97a5
guiialmeiida <gui.almeidacrz@gmail.com>
2020-11-11 07:17:20 -0300

    Criação do agendamento

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -7,6 +7,7 @@ import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 import AppError from '@shared/errors/AppError';
 interface IRequest {
   provider_id: string;
+  user_id: string;
   date: Date;
 }
 
@@ -17,7 +18,7 @@ class CreateAppointmentService {
     private appointmentsRepository: IAppointmentsRepository,
     ) {}
 
-  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
+  public async execute({ date, provider_id, user_id }: IRequest): Promise<Appointment> {
 
     const appointmentDate = startOfHour(date);
 
@@ -31,6 +32,7 @@ class CreateAppointmentService {
 
     const appointment = await this.appointmentsRepository.create({
       provider_id,
+      user_id,
       date: appointmentDate,
     });
 
